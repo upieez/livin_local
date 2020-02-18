3245a814951f2fa65d87d2ee121b553acc384010
upieez <samuelhww@gmail.com>
2020-02-18 10:35:29 +0800

    add a places controller

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,38 @@
+class PlacesController < ApplicationController
+    def index
+        @places = Place.all
+    end
+  
+    def show
+        @place = Place.find(params[:id])
+    end
+  
+    def new
+        @place = Place.new
+    end
+  
+    def edit
+    end
+  
+    def create
+        @place = Place.new(place_params)
+
+        @place.user = current_user
+
+        @place.save
+
+        redirect_to @place
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+
+    private
+    def place_params
+        params.require(:place).permit(:name, :description, :img_url, :address, :rating)
+    end
+
+end
