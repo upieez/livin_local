6d54bb79e654e5c658650cb17f9fa410d7c21c7e
upieez <samuelhww@gmail.com>
2020-02-18 10:38:09 +0800

    add routes for places

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_for :users
+    root 'places#index'
+    get '/places' => 'places#index', as: 'places'
+    get '/places/new' => 'places#new', as: 'new_place'
+    post '/places' => 'places#create'
+    get '/places/:id' => 'places#show' , as: 'place'
+    get '/places/:id/edit' => 'places#edit', as: 'edit_place'
+    patch '/places/:id' => 'places#update'
+    delete '/places/:id' => 'places#destroy'
 end
